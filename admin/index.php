4fe24efb5d5f9933f14b0d59cba4d64957148634
duongnhu10 <duongnhu03ctu@gmail.com>
2024-03-06 23:19:21 +0700

    upload file

---
+++ admin/index.php
@@ -0,0 +1,68 @@
+<html>
+
+<head>
+    <title>Agricultural - Page Home</title>
+    <link rel="stylesheet" href="/css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu">
+        <div class="wrapper">
+            <ul class="text-center">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Category</a></li>
+                <li><a href="#">Agricultural</a></li>
+                <li><a href="#">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu Section Ends -->
+
+    <!-- Main Content Section Starts -->
+    <div class="main-content">
+        <div class="wrapper">
+            <strong>DASHBOARD</strong>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Category
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Category
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Category
+            </div>
+
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                Category
+            </div>
+
+            <div class="clearfix">
+            </div>
+
+        </div>
+    </div>
+    <!-- Main Content Section Ends -->
+
+    <!-- Footer Section Starts -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2024 - CT299_06</p>
+        </div>
+    </div>
+    <!-- Footer Section Ends -->
+</body>
+
+</html>
